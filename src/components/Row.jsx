b6ccaa629e841ad112d9c269b3db81f4f88c49d6
atere21 <tosinateret@gmail.com>
2023-02-03 20:33:10 -0600

    netflix-clone

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Movie from './Movie';
 
-const Row =()=> {
+const Row = ({ title, fetchURL, }) => {
+    const [movies, setMovies] = useState([]);
+    
+    useEffect(() => {
+      axios.get(fetchURL).then((response) => {
+        setMovies(response.data.results);
+      });
+    }, [fetchURL]);
+
+    console.log(movies)
   return (
-    <div>Row</div>
+    <>
+     <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+      <div className='relative flex items-center '>
+        <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+         {movies.map((item, id) => (
+            <Movie key={id} item={item} />
+      
+          ))}
+
+        </div>
+      </div>
+
+    </>
   )
 }
 
 export default Row
+
+
+// import {FaHeart, FaRegHeart} from 'react-icons/fa'
+// import axios from 'axios';
+// import React, { useEffect, useState } from 'react';
+
+// const Row = ({ title, fetchURL, }) => {
+//     const [movies, setMovies] = useState([]);
+//     const [like, setLike] = useState (false)
+
+//     useEffect(() => {
+//       axios.get(fetchURL).then((response) => {
+//         setMovies(response.data.results);
+//       });
+//     }, [fetchURL]);
+
+//     console.log(movies)
+//   return (
+//     <>
+//      <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+//       <div className='relative flex items-center '>
+//         <div id={'slider'}>
+//          {movies.map((item, id) => (
+//             <div className='w-[160px] sm:w-[240px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+                
+//                    <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+//                      alt={item?.title}
+//                            />
+//                    <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white
+//                     '>
+//                         <p className='white-space-normal text-xs md:text-smfont-bold flex justify-center items-center h-full text-center'>{item.title}</p>
+//                          <p>
+//                             {like ? <FaHeart className='absolute top-4 text-gray-300'/>
+//                              : <FaRegHeart className='absolute top-4 text-gray-300'/>}
+                            
+//                          </p>
+//                    </div>
+//             </div>   
+      
+//           ))}
+
+//         </div>
+//       </div>
+
+
+//     </>
+//   )
+// }
+
+// export default Row
