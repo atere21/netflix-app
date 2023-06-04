db15b058b5ace8d50c3d4ce8cee6c1545af7436a
atere21 <you@tosinateret@gmail.com>
2023-06-04 04:23:26 -0700

    like-button-implented

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,8 +1,35 @@
 import React, { useState } from 'react'
 import {FaHeart, FaRegHeart} from 'react-icons/fa'
+import { UserAuth } from '../context/AuthContext'
+import { db } from '../firebase'
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore'
 
 const Movie = ({item})=> {
-    const [like, setLike] = useState (false)
+    const [like, setLike] = useState (false);
+    const [saved, setSaved] = useState(false)
+    const { user } = UserAuth();
+    
+    const movieID = doc[db, 'users', '${user?.email}']
+
+    const saveShow = async () => {
+      
+      if (user?.email) {
+        setLike(!like)
+        setSaved(true)
+        await updateDoc(movieID, {
+          savedShows: arrayUnion({
+            id: item.id,
+            img: item.title,
+            img: item.backdrop_path
+          })
+          
+        })
+      }
+      else {
+        alert('Please log in to save a movie');
+      }
+    }
+
 
   return (
     <div className='w-[160px] sm:w-[240px] md:w-[280px] lg:w-[280px] inline-block cursor-pointer relative p-2 '>
@@ -15,7 +42,7 @@ const Movie = ({item})=> {
                    <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white
                     '>
                         <p className='white-space-normal text-xs md:text-smfont-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
-                         <p>
+                         <p onClick={saveShow}>
                             {like ? (
                             <FaHeart className='absolute top-4 text-gray-300 '/>
                             )
